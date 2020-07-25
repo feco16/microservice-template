40434c048c3918b1c8d741a59037614b48fc1823
feco16 <ferenc.solyom1@gmail.com>
2020-07-25 21:14:17 +0300

    disable authentication

--- backend/ludisy-gateway/src/main/java/com/ludisy/ludisygateway/config/WebSecurityConfig.java
+++ backend/ludisy-gateway/src/main/java/com/ludisy/ludisygateway/config/WebSecurityConfig.java
@@ -19,7 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -56,16 +56,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable()
-                .authorizeRequests().antMatchers(authenticatePath).permitAll().
-                anyRequest().authenticated().and().
-
-// make sure we use stateless session; session won't be used to
-// store user's state.
-        exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-// Add a filter to validate the tokens with every request
-        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+//        httpSecurity.csrf().disable()
+//                .authorizeRequests().antMatchers(authenticatePath).permitAll().
+//                anyRequest().authenticated().and().
+//
+//// make sure we use stateless session; session won't be used to
+//// store user's state.
+//        exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//// Add a filter to validate the tokens with every request
+//        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
     }
 
