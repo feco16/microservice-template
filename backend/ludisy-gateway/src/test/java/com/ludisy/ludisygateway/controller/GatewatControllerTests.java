254432a5a70605e1f7f611d4ac838c725af97c9c
Ferenc.Solyom2 <ferenc.solyom2@microfocus.com>
2021-01-10 18:07:35 +0200

    Basic testing ready

--- backend/ludisy-gateway/src/test/java/com/ludisy/ludisygateway/controller/GatewatControllerTests.java
+++ backend/ludisy-gateway/src/test/java/com/ludisy/ludisygateway/controller/GatewatControllerTests.java
@@ -3,11 +3,12 @@ package com.ludisy.ludisygateway.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ludisy.ludisygateway.SERVICE_UserManagement.dto.ApplicationUserDTO;
 import com.ludisy.ludisygateway.SERVICE_UserManagement.service.ApplicationUserService;
+import com.ludisy.ludisygateway.SERVICE_WorkoutManagement.dto.WorkoutDTO;
+import com.ludisy.ludisygateway.SERVICE_WorkoutManagement.service.WorkoutService;
 import com.ludisy.ludisygateway.config.AppControllerAdvice;
 import com.ludisy.ludisygateway.rest.GatewayController;
 import com.ludisy.ludisygateway.shared.NotFoundException;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -18,12 +19,18 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static com.ludisy.ludisygateway.TestConstants.BASE_REQUEST_URI;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -42,6 +49,9 @@ public class GatewatControllerTests {
 
     @Mock
     ApplicationUserService applicationUserService;
+
+    @Mock
+    WorkoutService workoutService;
 
     @Before
     public void setup() {
@@ -88,27 +98,96 @@ public class GatewatControllerTests {
     @Test
     public void getUserByIdNotFound() throws Exception {
 
-        String uuid = UUID.randomUUID().toString();
-        doThrow(NotFoundException.class).when(applicationUserService).getDTOById(uuid);
+        doThrow(NotFoundException.class).when(applicationUserService).getDTOById(any());
 
         mockMvc.
-                perform(get(BASE_REQUEST_URI + "/user/" + uuid))
+                perform(get(BASE_REQUEST_URI + "/user/any-uuid"))
                 .andExpect(status().isNotFound());
-
     }
 
-    // TODO test all endpoints
-    @Ignore
+    @Test
+    public void deleteWorkoutsByUserId() throws Exception {
+
+        doAnswer((i) -> null).when(applicationUserService).deleteWorkoutsByUserId(anyString());
+
+        mockMvc.
+                perform(delete(BASE_REQUEST_URI + "/user/any-uuid/workouts"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void postWorkout() throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        String uuid = UUID.randomUUID().toString();
+        WorkoutDTO workoutDTO = new WorkoutDTO();
+        doReturn(workoutDTO)
+                .when(workoutService)
+                .createWorkout(any(), any());
+
+        mockMvc.
+                perform(post(BASE_REQUEST_URI + "/user/" + uuid + "/workout")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(workoutDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void postWorkoutUserIdNotFound() throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        doThrow(NotFoundException.class)
+                .when(workoutService)
+                .createWorkout(any(), any());
+
+        mockMvc.
+                perform(post(BASE_REQUEST_URI + "/user/any-uuid/workout")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(new WorkoutDTO())))
+                .andExpect(status().isNotFound());
+    }
+
     @Test
     public void getWorkoutsByTimestamp() throws Exception {
 
         String uuid = UUID.randomUUID().toString();
+        List<WorkoutDTO> workoutDTOList = new ArrayList<>();
+        doReturn(workoutDTOList)
+                .when(workoutService)
+                .getWorkoutsByTimestamp(any(), any(), any());
+
         mockMvc.
-                perform(get(BASE_REQUEST_URI + "/user/" + uuid + "/workouts"))
-                .andDo(print())
+                perform(get(BASE_REQUEST_URI + "/user/" + uuid + "/workouts")
+                        .param("startingDay", "1")
+                        .param("endingDay", "1")
+                )
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON));
 
+    }
+
+    @Test
+    public void getWorkoutsByTimestampMissingParams() throws Exception {
+
+        String error = mockMvc.
+                perform(get(BASE_REQUEST_URI + "/user/any-uuid/workouts"))
+                .andExpect(status().isBadRequest())
+                .andReturn().getResolvedException().getMessage();
+
+        assertEquals("Required Long parameter 'startingDay' is not present", error);
+
+    }
+
+    @Test
+    public void pageNotFound() throws Exception {
+
+        mockMvc.
+                perform(get(BASE_REQUEST_URI + "/bad-url"))
+                .andExpect(status().isNotFound());
     }
 
 }
